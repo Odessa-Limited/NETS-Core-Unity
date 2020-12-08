610ff24ec56d79f37546c47cefb9d5bcaed7ebae
Smashingtonn <mitchell.bartlett22@gmail.com>
2020-12-09 10:35:14 +1300

    Create NETS obj at run time

--- Runtime/Core/NetsInitialization.cs
+++ Runtime/Core/NetsInitialization.cs
@@ -1,18 +1,17 @@
 ﻿#if UNITY_EDITOR
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace OdessaEngine.NETS.Core {
-    class NetsInitialization {
-        [UnityEditor.Callbacks.DidReloadScripts]
-        private static void OnScriptsReloaded() {
-            var settingsFile = $"{Directory.GetCurrentDirectory()}/ProjectSettings/NETS.conf";
-            if (!File.Exists(settingsFile)) {
-                File.Create(settingsFile).Close();
-                if (Object.FindObjectsOfType<NetsNetworking>().Length == 0) {
-                    var go = new GameObject("NETS");
-                    go.AddComponent<NetsNetworking>();
-                }
+    public class NetsInitialization {
+
+        [RuntimeInitializeOnLoadMethod]
+        static void OnRuntimeMethodLoad() {
+            if (Object.FindObjectsOfType<NetsNetworking>().Length == 0) {
+                var go = new GameObject("NETS");
+                go.AddComponent<NetsNetworking>();
+                
             }
         }
     }
